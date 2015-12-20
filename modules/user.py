e28e834bbce1490aa2c9e56e5c395749bb1bf3ba
Christopher Strecker <chris@foldl.de>
2015-12-20 09:48:13 +0100

    abstraction for get_albums

--- modules/user.py
+++ modules/user.py
@@ -17,14 +17,7 @@ def index(username):
     if username != current_user.name:
         return redirect(url_for('user.index', username=current_user.name))
 
-    albums = []
-    with db.pg_connection(config['app-database']) as (_, cur, err):
-        if not err:
-            user = db.query_one(cur, 'SELECT id_user FROM app.user WHERE user_name=%(name)s;', {'name': username})
-            albums = db.query_all(
-                cur,
-                'SELECT id_album, album_title FROM app.album WHERE fk_user=%(user)s',
-                {'user': user.id_user})
+    albums = get_albums(current_user.name)
 
     env = {
         'module': 'User',
@@ -32,3 +25,16 @@ def index(username):
         'albums': albums
     }
     return render_template('user.html', **env)
+
+
+# TODO: move function to somewhere else?
+def get_albums(user_name):
+    albums = []
+    with db.pg_connection(config['app-database']) as (_, cur, err):
+        if not err:
+            user = db.query_one(cur, 'SELECT id_user FROM app.user WHERE user_name=%(name)s;', {'name': user_name})
+            albums = db.query_all(
+                cur,
+                'SELECT id_album, album_title FROM app.album WHERE fk_user=%(user)s',
+                {'user': user.id_user})
+    return albums
