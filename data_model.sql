70311cef19197c2a1f40440cd9910379fdb08053
Christopher Strecker <chris@foldl.de>
2015-12-24 06:44:03 +0100

    last_modified column

--- data_model.sql
+++ data_model.sql
@@ -5,15 +5,34 @@ CREATE EXTENSION IF NOT EXISTS pgcrypto;
 DROP SCHEMA IF EXISTS app CASCADE;
 CREATE SCHEMA app;
 
+DROP SCHEMA IF EXISTS fn CASCADE;
+CREATE SCHEMA fn;
+
+
+-------------------------------------------------------------------------------
+
+
+CREATE OR REPLACE FUNCTION fn.update_last_modified()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.last_modified = clock_timestamp();
+  RETURN NEW;
+END;
+$$ language 'plpgsql';
+
+
+-------------------------------------------------------------------------------
 
 
 CREATE TABLE app.user (
-  id_user   SERIAL PRIMARY KEY,
-  user_name TEXT NOT NULL UNIQUE,
-  email     TEXT NOT NULL UNIQUE,
-  pw_hash   TEXT NOT NULL
+  id_user       SERIAL PRIMARY KEY,
+  user_name     TEXT NOT NULL UNIQUE,
+  email         TEXT NOT NULL UNIQUE,
+  pw_hash       TEXT NOT NULL,
+  last_modified TIMESTAMP WITH TIME ZONE NOT NULL
 );
-
+CREATE TRIGGER app_user_update_last_modified BEFORE INSERT OR UPDATE ON app.USER
+  FOR EACH ROW EXECUTE PROCEDURE fn.update_last_modified();
 CREATE INDEX ON app.user (user_name);
 
 INSERT INTO app.user (user_name, email, pw_hash) VALUES
@@ -21,34 +40,51 @@ INSERT INTO app.user (user_name, email, pw_hash) VALUES
 
 
 
-CREATE TABLE app.album (
-  id_album    SERIAL PRIMARY KEY,
-  album_title TEXT NOT NULL,
-  fk_user     INTEGER NOT NULL
+CREATE TABLE app.signup (
+  email         TEXT NOT NULL UNIQUE,
+  is_user       BOOLEAN NOT NULL,
+  last_modified TIMESTAMP WITH TIME ZONE NOT NULL
 );
+CREATE TRIGGER app_signup_update_last_modified BEFORE INSERT OR UPDATE ON app.signup
+  FOR EACH ROW EXECUTE PROCEDURE fn.update_last_modified();
 
+
+
+CREATE TABLE app.album (
+  id_album      SERIAL PRIMARY KEY,
+  album_title   TEXT NOT NULL,
+  fk_user       INTEGER NOT NULL,
+  last_modified TIMESTAMP WITH TIME ZONE NOT NULL
+);
+CREATE TRIGGER app_album_update_last_modified BEFORE INSERT OR UPDATE ON app.album
+  FOR EACH ROW EXECUTE PROCEDURE fn.update_last_modified();
 ALTER TABLE app.album ADD FOREIGN KEY (fk_user) REFERENCES app.user (id_user);
 ALTER TABLE app.album ADD UNIQUE (album_title, fk_user);
 
 
-CREATE TABLE app.item (
-  id_item     SERIAL PRIMARY KEY,
-  fk_album    INTEGER NOT NULL,
-  image       TEXT NOT NULL,
-  ts          TIMESTAMP WITH TIME ZONE,
-  lat         NUMERIC(9, 6),
-  lon         NUMERIC(9, 6),
-  description TEXT
-);
 
+CREATE TABLE app.item (
+  id_item       SERIAL PRIMARY KEY,
+  fk_album      INTEGER NOT NULL,
+  image         TEXT NOT NULL,
+  ts            TIMESTAMP WITH TIME ZONE,
+  lat           NUMERIC(9, 6),
+  lon           NUMERIC(9, 6),
+  description   TEXT,
+  last_modified TIMESTAMP WITH TIME ZONE NOT NULL
+);
+CREATE TRIGGER app_item_update_last_modified BEFORE INSERT OR UPDATE ON app.item
+  FOR EACH ROW EXECUTE PROCEDURE fn.update_last_modified();
 ALTER TABLE app.item ADD FOREIGN KEY (fk_album) REFERENCES app.album (id_album);
 
 
 
 CREATE TABLE app.share_type (
   id_share_type   SERIAL PRIMARY KEY,
-  share_type_name TEXT
+  share_type_name TEXT,
+  last_modified   TIMESTAMP WITH TIME ZONE NOT NULL
 );
+CREATE TRIGGER app_type_update_last_modified BEFORE INSERT OR UPDATE ON app.share_type FOR EACH ROW EXECUTE PROCEDURE fn.update_last_modified();
 
 
 
@@ -57,9 +93,11 @@ CREATE TABLE app.share (
   fk_share_type SMALLINT NOT NULL,
   url           TEXT,
   email         TEXT,
-  fk_user       INTEGER
+  fk_user       INTEGER,
+  last_modified TIMESTAMP WITH TIME ZONE NOT NULL
 );
-
+CREATE TRIGGER app_share_update_last_modified BEFORE INSERT OR UPDATE ON app.share
+  FOR EACH ROW EXECUTE PROCEDURE fn.update_last_modified();
 ALTER TABLE app.share ADD FOREIGN KEY (fk_album) REFERENCES app.album (id_album);
 ALTER TABLE app.share ADD FOREIGN KEY (fk_share_type) REFERENCES app.share_type (id_share_type);
 ALTER TABLE app.share ADD FOREIGN KEY (fk_user) REFERENCES app.user (id_user);
