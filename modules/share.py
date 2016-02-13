24c40885018b8b2370861057cabc7c7c2948806c
Christopher Strecker <chris@foldl.de>
2016-02-13 02:01:09 +0100

    bugfix: ignore deleted albums for share settings

--- modules/share.py
+++ modules/share.py
@@ -43,6 +43,7 @@ FROM travel_log.album,
      travel_log.user,
      travel_log.share_type
 WHERE album.id_album = share.fk_album AND album.album_title = %(album)s
+  AND NOT album.is_deleted
   AND "user".id_user = share.fk_user AND "user".user_name = %(user)s
   AND share_type.share_type_name = %(share)s
 ''',
@@ -62,6 +63,7 @@ JOIN travel_log.share_type st ON st.id_share_type = s.fk_share_type
 JOIN travel_log.user u ON u.id_user = s.fk_user
 JOIN travel_log.album a ON a.id_album = s.fk_album
 WHERE u.user_name = %(user)s AND a.album_title = %(album)s
+  AND NOT a.is_deleted
             ''',
             {'user': user_name, 'album': album_title}
         )
