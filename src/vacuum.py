af6e9bbead35c4b1769fd5d1c7e7973979829455
Christopher Strecker <chris@foldl.de>
2016-03-20 14:41:14 +0100

    log vacuum runs

--- src/vacuum.py
+++ src/vacuum.py
@@ -1,12 +1,31 @@
 import glob
+import logging
+from logging.handlers import TimedRotatingFileHandler
 import os
 
 import shared.db as db
 from shared.util import config
 
+def get_logger():
+    log_folder = config['log-folder']
+    if not os.path.isdir(log_folder):
+        os.makedirs(log_folder)
+
+    logger = logging.getLogger(__name__)
+    if not getattr(logger, 'has_handler', False):
+        logger.setLevel(logging.INFO)
+        handler = TimedRotatingFileHandler(os.path.join(log_folder, 'vacuum.log'), when='midnight', interval=1, utc=True)
+        formatter = logging.Formatter('"%(asctime)s";"%(message)s"')
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        logger.has_handler = True
+
+    return logger
+
 
 def vacuum():
-    # TODO: log deleted filenames
+    logger = get_logger()
+    logger.info('[Start][Vacuum]')
 
     # Step 1:
     # Search for database entries that have the `is_deleted` flag. If
@@ -25,11 +44,11 @@ SELECT
 FROM travel_log.album a
 LEFT JOIN travel_log.item i ON a.id_album = i.fk_album
 WHERE a.is_deleted OR i.is_deleted
-'''
+            '''
         )
 
-    album_ids = tuple([int(item.id_album) for item in stale_items if item.id_album])
-    item_ids = tuple([int(item.id_item) for item in stale_items if item.id_item])
+    album_ids = tuple(set([int(item.id_album) for item in stale_items if item.id_album]))
+    item_ids = tuple(set([int(item.id_item) for item in stale_items if item.id_item]))
     for item in stale_items:
         found_stale_items = True
         if item.id_item:
@@ -37,6 +56,7 @@ WHERE a.is_deleted OR i.is_deleted
                 config['storage-engine']['path'],
                 item.image
             )
+            logger.info('[Delete][File][Item]: %s', fs_path)
             print('  - Deleting file %s' % fs_path)
             if os.path.isfile(fs_path):
                 os.remove(fs_path)
@@ -52,18 +72,21 @@ WHERE a.is_deleted OR i.is_deleted
     with db.pg_connection(config['app-database']) as (_, cur, _):
         if item_ids:
             found_db_entries = True
+            logger.info('[Delete][Database][Item]: %s', item_ids)
             print('  - Deleting items')
             cur.execute(
                 'DELETE FROM travel_log.item WHERE id_item IN %(items)s',
                 {'items': item_ids}
             )
         if album_ids:
+            logger.info('[Delete][Database][Share]: %s', album_ids)
             print('  - Deleting share entries')
             cur.execute(
                 'DELETE FROM travel_log.share WHERE fk_album IN %(albums)s',
                 {'albums': album_ids}
             )
             found_db_entries = True
+            logger.info('[Delete][Database][Album]: %s', album_ids)
             print('  - Deleting albums')
             cur.execute(
                 'DELETE FROM travel_log.album WHERE id_album IN %(albums)s',
@@ -102,6 +125,7 @@ SELECT background FROM travel_log.album WHERE NOT is_deleted
     diff = on_disk.difference(ref)
     for stale in diff:
         found_stale_files = True
+        logger.info('[Delete][Filesystem][Image]: %s', stale)
         print(' - Deleting image from disk: %s' % stale)
         os.remove(stale)
     if not found_stale_files:
@@ -124,10 +148,13 @@ SELECT background FROM travel_log.album WHERE NOT is_deleted
         rest, uid = os.path.split(rest)
         if (int(uid), int(aid)) not in albums:
             found_stale_folders = True
+            logger.info('[Delete][Filesystem][Folder]: %s', f)
             print(' - Delete folder from disk: %s' % f)
             os.rmdir(f)
     if not found_stale_folders:
         print(' - None found')
+
+    logger.info('[Stop][Vacuum]')
 
 
 if __name__ == '__main__':
